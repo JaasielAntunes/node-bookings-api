e4b5acd4240fb7250947a3ee5053a338beaf67f2
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-01-23 10:25:10 -0300

    Classe para autenticação de usuário

---
+++ src/auth/User.js
@@ -0,0 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
+
+class User {
+  constructor({id, name, email, password}) {
+    this.id = id ?? uuidv4();
+    this.name = name;
+    this.email = email;
+    this.password = password;
+  }
+}
+
+export { User }
