ca9b4e60a7bb2ebf7f585e2e3069770f8c98e2b5
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-01-23 17:36:29 -0300

    Utilização de tokens jwt

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -9,25 +9,22 @@ class AuthController {
     if (!name || !email || !password) {
       return {
         code: 400,
-        body: { message: "Todos os campos devem ser informados!" },
+        body: { message: "Todos os campos devem ser preenchidos!" },
       };
+    }
 
-      try {
-        const user = this.service.register(name, email, password);
-        return {
-          code: 201,
-          body: { message: "Usário criado com sucesso!", user },
-        };
-      } catch (e) {
-        return {
-          code: 400,
-          body: { message: e.message }
-        };
-      }
+    try {
+      const user = this.service.register(name, email, password);
+      return { code: 201, body: user };
+      
+    } catch (e) {
+      return { code: 400, body: { message: e.message } };
     }
   }
 
   login() {
-    
+
   }
 }
+
+export { AuthController };
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,15 +1,16 @@
 import { User } from "../auth/User.js";
-import { bcrypt } from "bcrypt";
-import { jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 class UserService {
   constructor(repo) {
     this.repo = repo;
   }
 
-  register({ name, email, password }) {
+  register(name, email, password) {
     const existingUser = this.repo.findByEmail(email);
     if (existingUser) throw new Error("Este email ja existe!");
+
     const user = new User({ name, email, password });
     user.password = bcrypt.hashSync(user.password, 10);
     this.repo.save(user);
@@ -22,9 +23,13 @@ class UserService {
 
     const isPasswordValid = bcrypt.compareSync(password, user.password);
     if (!isPasswordValid) throw new Error("Senha inválida!");
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      "segredo-jwt",
+      { expiresIn: "1d" }
+    );
     
-    const token = jwt.sign({ id: user.id, email: user.email }, 
-      process.env.JWT_SECRET, { expiresIn: "1d" });
     return { user, token };
   }
 }
