bf19f60babd5465dad71c183306bc574c4731b00
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-01-24 12:57:25 -0300

    Retorno de usuário cadastrado e logado com sucesso

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -14,7 +14,10 @@ class UserService {
     const user = new User({ name, email, password });
     user.password = bcrypt.hashSync(user.password, 10);
     this.repo.save(user);
-    return user;
+    return {
+      code: 201,
+      body: { message: "Usuário cadastrado com sucesso!", user },
+    };
   }
 
   login(email, password) {
@@ -24,12 +27,19 @@ class UserService {
     const isPasswordValid = bcrypt.compareSync(password, user.password);
     if (!isPasswordValid) throw new Error("Senha inválida!");
 
-    const token = jwt.sign({ email: user.email }, "segredo-jwt", {
+    const token = jwt.sign({ id: user.id, email: user.email }, "segredo-jwt", {
       expiresIn: "1d",
     });
 
     const { password: userPassword, ...userWithoutPassword } = user;
-    return { user: userWithoutPassword, token };
+    return {
+      code: 201,
+      body: {
+        message: "Usuário logado com sucesso!",
+        user: userWithoutPassword,
+        token,
+      },
+    };
   }
 
   verifyToken(token) {
